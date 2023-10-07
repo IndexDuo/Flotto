f6cdb553615321cffff8e18ecd8f5e2d7770ea99
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2023-10-07 20:26:05 +0000

    Oct 7, 2023, 4:26 PM

--- mongoDB/importData.js
+++ mongoDB/importData.js
@@ -5,7 +5,8 @@ import fs from 'fs/promises'; // Use fs/promises for async file operations
 const uri = "mongodb+srv://indexduo:index2012512@flottery.c5klhwf.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
 
 // JSON file containing winningNumbers data
-const jsonFilePath = './dataJSON/winningNumbers.json';
+const winningNumberjsonFilePath = './dataJSON/winningNumbers.json';
+const lotteryResultjsonFilePath = './dataJSON/lottery-result.json';
 
 async function importData() {
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,18 +16,26 @@ async function importData() {
     console.log('Connected to MongoDB Atlas');
 
     const database = client.db('florida_lottery');
-    const collection = database.collection('winningNumbers');
+    const collectionWin = database.collection('winningNumbers');
+    const collectionLottery = database.collection('lotteryResult');
 
     // Read JSON data from file
-    const rawData = await fs.readFile(jsonFilePath, 'utf8');
-    const data = JSON.parse(rawData);
+    const rawDataWin = await fs.readFile(winningNumberjsonFilePath, 'utf8');
+    const dataWin = JSON.parse(rawDataWin);
+
+    const rawDataResult = await fs.readFile(lotteryResultjsonFilePath, 'utf8');
+    const dataResult = JSON.parse(rawDataResult);
 
     // Delete all existing documents in the collection
-    await collection.deleteMany({});
+    await collectionWin.deleteMany({});
+    await collectionLottery.deleteMany({});
 
     // Insert new data into MongoDB
-    const result = await collection.insertMany(data);
-    console.log(`${result.insertedCount} documents inserted`);
+    const result = await collectionWin.insertMany(dataWin);
+    console.log(`${result.insertedCount} documents inserted into winningNumbers collection`);
+
+    const result2 = await collectionLottery.insertMany(dataResult);
+    console.log(`${result2.insertedCount} documents inserted into lotteryResult collection`);
   } catch (err) {
     console.error('Error:', err);
   } finally {
