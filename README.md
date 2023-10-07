2624ab9f65ac5dfe2dcf345f54c8449c560ac544
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2023-10-07 19:10:14 +0000

    Oct 7, 2023, 3:10 PM

--- README.md
+++ README.md
@@ -3,19 +3,18 @@
 ## Getting Started
 
 1. clone this repo to your vscode
-2. do  npm i in your terminal
-3. to run the .js files,  do node "file-name"
+2. do npm i in your terminal
+3. to run the .js files, do node "file-name"
 
 ### What's each file for?
 
 #### pdf.js
 
 This .js file turns the **lottowinners_floridalotto.pdf** to raw data and then do some regex stuff to make it prettier.
- 
+
 The result is updated in the **pdfoutput.txt** file.
 
 Every time you do **node pdf.js** in you terminal, **pdfoutput.txt** will be updated.
-
 
 ## How to push my code so it's in the main branch
 
--- objectdataconversion.js
+++ objectdataconversion.js
@@ -1,47 +1,46 @@
 // objectdataconversion.mjs
-import { exec } from 'child_process';
+import { exec } from 'child_process'
 
-import fs from 'fs';
+import fs from 'fs'
 
 function convertDataToObject() {
-    // Your data conversion logic here
-    
-    // read the pdfoutput.txt file
-    fs.readFile('pdfoutput.txt', 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Error reading pdfoutput.txt: ${err}`);
-            return;
-        }
-        // console.log(data);
-        // sort the data into an object. The object should have the following structure: {
-//   "date": "8/30/2023"
-//   "buyer": "EXEMPT PURSUANT TO F.S.24.1051",
-//   "seller": "TOWN STAR #491",
-//   "jackpot": "$3,750,000",
-//   "pay": "CASH OPTION",
-//   "prize": "$2,099,143.75",
-//   "quickPlay": false,
-//   "tickets": 1,
-//   "buyerAddress": "SEBRING, FL 33871",
-//   "sellerAddress": "1865 HIGHWAY 70 WEST, OKEECHOBEE"
-// } 
-//
-        // write the object to a .json file (output.json)
-        fs.writeFileSync('output.json', JSON.stringify(data));
-        console.log('Updated output.json file.');
+  // Your data conversion logic here
 
-    });
+  // read the pdfoutput.txt file
+  fs.readFile('pdfoutput.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading pdfoutput.txt: ${err}`)
+      return
+    }
+    // console.log(data);
+    // sort the data into an object. The object should have the following structure: {
+    //   "date": "8/30/2023"
+    //   "buyer": "EXEMPT PURSUANT TO F.S.24.1051",
+    //   "seller": "TOWN STAR #491",
+    //   "jackpot": "$3,750,000",
+    //   "pay": "CASH OPTION",
+    //   "prize": "$2,099,143.75",
+    //   "quickPlay": false,
+    //   "tickets": 1,
+    //   "buyerAddress": "SEBRING, FL 33871",
+    //   "sellerAddress": "1865 HIGHWAY 70 WEST, OKEECHOBEE"
+    // }
+    //
+    // write the object to a .json file (output.json)
+    fs.writeFileSync('output.json', JSON.stringify(data))
+    console.log('Updated output.json file.')
+  })
 }
 
 // Export functions or variables as needed
-export { convertDataToObject };
+export { convertDataToObject }
 
 // Run pdf.js script to generate pdfoutput.txt
 exec('node pdf.js', (error, stdout, stderr) => {
   if (error) {
-    console.error(`Error running pdf.mjs: ${error}`);
-    return;
+    console.error(`Error running pdf.mjs: ${error}`)
+    return
   }
   // Continue with data conversion once pdf.mjs is done
-  convertDataToObject();
-});
+  convertDataToObject()
+})
