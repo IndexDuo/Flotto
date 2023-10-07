bb76f920f47ab1a4fa1d91dc2bff54c754c985c6
lindsey-nielsen <122649641+lindsey-nielsen@users.noreply.github.com>
2023-10-07 17:40:45 +0000

    Oct 7, 2023, 1:40 PM

--- convert.js
+++ convert.js
@@ -4,74 +4,64 @@ import fs from 'fs';
 // Run pdf.js using child_process module
 exec('node pdf.js', (error, stdout, stderr) => {
     if (error) {
-        console.error('Error running pdf.js: ${error.message}');
+        console.error(`Error running pdf.js: ${error.message}`);
         return;
     }
 
     // read data from pdfoutput.txt
     fs.readFile('pdfoutput.txt', 'utf8', (err, data) => {
         if (err) {
-            console.error('Error reading pdfoutput.txt: ${err.message}');
+            console.error(`Error reading pdfoutput.txt: ${err.message}`);
             return;
         }
 
-        // process data (parse it, convert it to object)
-        try {
-            const processedData = processData(data);
+        // Split the data into entries based on the date
+        const entries = data.split(/\d{2}\/\d{2}\/\d{4}/); // Assumes date format MM/DD/YYYY
 
-            // write processed data to lottery-result.json
-            fs.writeFile('lottery-result.json', JSON.stringify(processedData, null, 2), (err) => {
-                if (err) {
-                    console.error('Error writing to lottery-result.json: ${err.message}');
-                    return;
-                }
+        // Filter out any empty entries
+        const validEntries = entries.filter(entry => entry.trim().length > 0);
 
-                console.log('Data has been converted and saved to lottery-result.json');
-            })
-        } catch (e) {
-            console.error('Error processing data: ${e.message}');
-        }
+        // Process each entry
+        const results = validEntries.map(processData);
+
+        // Flatten the array of results if needed
+        const flattenedResults = [].concat(...results);
+
+        // Write processed data to lottery-result.json
+        fs.writeFile('lottery-result.json', JSON.stringify(flattenedResults, null, 2), (err) => {
+            if (err) {
+                console.error(`Error writing to lottery-result.json: ${err.message}`);
+                return;
+            }
+
+            console.log('Data has been converted and saved to lottery-result.json');
+        });
     });
-}); 
+});
 
 // define data processing logic
-function processData(data) {
-    const result = [];
+function processData(entry) {
+    const entryData = {};
 
-    // split the data into individual entries
-    const entries = data.split("Entry:");
+    // Split the entry into lines
+    const lines = entry.trim().split('\n');
 
-    // interate through each entry and process it
-    for (const entry of entries) {
-        const entryData = {};
-
-        // extract info from the entry
-        const lines = entry.trim().split('\n');
-
-        console.log("data buyer is");
-        console.log(lines[1].trim());
-        
-        // Check if the entry is unclaimed or expired, and skip it
-        if (lines[1].trim() === 'UNCLAIMED AT THIS TIME' || lines[1].trim() === 'EXPIRED') {
-            console.log("in if statement");
-            continue; // Skip this entry and move to the next one
-        }
-
-        // process lines and populate entryData with desired fields
-        entryData.date = lines[0].trim();       // Date
-        entryData.buyer = lines[1].trim();      // Buyer
-        entryData.seller = lines[2].trim();     // Seller
-        entryData.jackpot = lines[3].trim();    // Jackpot
-        entryData.pay = lines[4].trim();        // Pay
-        entryData.prize = lines[5].trim();      // Prize
-        entryData.quickPlay = lines[6].trim() === "Quick Pick"; //QuickPlay
-        entryData.tickets = parseInt(lines[7].trim());  // Tickets
-        entryData.buyerAddress = lines[8].trim();  // Buyer Address
-        entryData.sellerAddress = lines[9].trim();  // Seller Address
-
-        // Add the processed entryData to the result array
-        result.push(entryData);
+    // Check if the entry is unclaimed or expired, and skip it
+    if (lines[1].trim() === 'UNCLAIMED AT THIS TIME' || lines[1].trim() === 'EXPIRED') {
+        return null; // Skip this entry and move to the next one
     }
 
-    return result;
+    // Process lines and populate entryData with desired fields
+    entryData.date = lines[0].trim();       // Date
+    entryData.buyer = lines[1].trim();      // Buyer
+    entryData.seller = lines[2].trim();     // Seller
+    entryData.jackpot = lines[3].trim();    // Jackpot
+    entryData.pay = lines[4].trim();        // Pay
+    entryData.prize = lines[5].trim();      // Prize
+    entryData.quickPlay = lines[6].trim() === "Quick Pick"; //QuickPlay
+    entryData.tickets = parseInt(lines[7].trim());  // Tickets
+    entryData.buyerAddress = lines[8].trim();  // Buyer Address
+    entryData.sellerAddress = lines[9].trim();  // Seller Address
+
+    return entryData;
 }
