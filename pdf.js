d9f7a3cc4dba3c03a33da5fbc714581cde39a1b3
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2023-10-07 07:13:41 +0000

    Oct 7, 2023, 3:13 AM

--- pdf.js
+++ pdf.js
@@ -52,7 +52,7 @@ pdf(pdfDataBuffer)
         currentSection = line;
       } else {
         // Append the current line to the current section
-        currentSection += ` ${line}`;
+        currentSection += `\n${line}`;
       }
     }
 
