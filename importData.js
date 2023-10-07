13d6308c06b1c24a2d519baf5076e9d1f2ca4ed8
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2023-10-07 19:13:54 +0000

    Oct 7, 2023, 3:13 PM

---
+++ importData.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+import fs from 'fs/promises'; // Use fs/promises for async file operations
+
+// MongoDB Atlas cluster connection string
+const uri = '';
+
+// JSON file containing winningNumbers data
+const jsonFilePath = 'winningNumbers.json';
+
+async function importData() {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+
+    const database = client.db('your-database-name');
+    const collection = database.collection('winningNumbers');
+
+    // Read JSON data from file
+    const rawData = await fs.readFile(jsonFilePath, 'utf8');
+    const data = JSON.parse(rawData);
+
+    // Insert data into MongoDB
+    const result = await collection.insertMany(data);
+    console.log(`${result.insertedCount} documents inserted`);
+  } catch (err) {
+    console.error('Error:', err);
+  } finally {
+    // Close the MongoDB connection
+    client.close();
+  }
+}
+
+// Call the importData function to start the import
+importData();
