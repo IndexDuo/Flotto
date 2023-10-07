341f47a7d8ac3263b499be57316a231f98f92a70
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2023-10-07 16:02:50 +0000

    Oct 7, 2023, 12:02 PM

--- objectdataconversion.js
+++ objectdataconversion.js
@@ -12,7 +12,24 @@ function convertDataToObject() {
             console.error(`Error reading pdfoutput.txt: ${err}`);
             return;
         }
-        console.log(data);
+        // console.log(data);
+        // sort the data into an object. The object should have the following structure: {
+//   "date": "8/30/2023"
+//   "buyer": "EXEMPT PURSUANT TO F.S.24.1051",
+//   "seller": "TOWN STAR #491",
+//   "jackpot": "$3,750,000",
+//   "pay": "CASH OPTION",
+//   "prize": "$2,099,143.75",
+//   "quickPlay": false,
+//   "tickets": 1,
+//   "buyerAddress": "SEBRING, FL 33871",
+//   "sellerAddress": "1865 HIGHWAY 70 WEST, OKEECHOBEE"
+// } 
+//
+        // write the object to a .json file (output.json)
+        fs.writeFileSync('output.json', JSON.stringify(data));
+        console.log('Updated output.json file.');
+
     });
 }
 
