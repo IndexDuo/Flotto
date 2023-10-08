2b2fec6cb299d288defe81a1272d62cf3432de4a
lindsey-nielsen <122649641+lindsey-nielsen@users.noreply.github.com>
2023-10-08 05:36:14 +0000

    Oct 8, 2023, 1:36 AM

--- lotteryApi.js
+++ lotteryApi.js
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename); // Get the directory of the current 
 const app = express();
 const port = 3000;
 app.use(express.static('public'));
+app.use(express.json());
 
 // MongoDB Atlas cluster connection string
 const uri = 'mongodb+srv://indexduo:index2012512@flottery.c5klhwf.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp';
@@ -165,23 +166,59 @@ app.get('/getData/winResults', async (req, res) => {
         tickets: item.tickets
     }));
 
-app.post('/calculateWinningChance', async (req, res) => {
-    try {
-        const selectedNumbers = req.body.selectedNumbers.split(',').map(Number);
-
-        // Retrieve the count of past winning numbers that match the user's input
-        const matchingNumbersCount = await florida_lottery.winningNumbers({ numbers: { $all: selectedNumbers } });
-
-        // Calculate the winning chance as a percentage
-        const totalDrawings = await florida_lottery.winningNumbers();
-        const chance = (matchingNumbersCount / totalDrawings) * 100;
-
-        res.json({ chance });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
+    app.post('/calculateWinningChance', async (req, res) => {
+        try {
+            const selectedNumbers = req.body.selectedNumbers.split(',').map(Number);
+    
+            client = new MongoClient(uri, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+    
+            await client.connect();
+            console.log('Connected to MongoDB Atlas');
+    
+            const database = client.db('florida_lottery');
+            const collection = database.collection('winningNumbers'); // Reference to the collection
+    
+            const statistics = {}; // Store the statistics data
+    
+            // Query your MongoDB Atlas collection for statistics data
+            const rawData = await collection.find({}).toArray();
+    
+            // Filter the data for numbers and times
+            rawData.forEach((item) => {
+                statistics[item.number] = {
+                    times: item.times,
+                    lastDrawnDate: item.lastDrawnDate,
+                    percentageOfDrawings: item.percentageOfDrawings,
+                };
+            });
+    
+            // Calculate the total count of matching numbers and the total drawings
+            let matchingNumbersCount = 0;
+            const totalDrawings = rawData.length;
+    
+            selectedNumbers.forEach((number) => {
+                if (statistics[number]) {
+                    matchingNumbersCount += statistics[number].times;
+                }
+            });
+    
+            // Calculate the winning chance as a percentage
+            const chance = (matchingNumbersCount / totalDrawings) * 100;
+    
+            res.json({ chance });
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        } finally {
+            if (client) {
+                // Close the MongoDB connection if 'client' is defined
+                client.close();
+            }
+        }
+    });
   
       // Respond with the retrieved data as JSON
       res.status(200).json(filteredData);
