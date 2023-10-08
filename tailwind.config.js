013b33648d3120efbef25632fdcebe61a380c306
lindsey-nielsen <122649641+lindsey-nielsen@users.noreply.github.com>
2023-10-08 02:43:44 +0000

    Oct 7, 2023, 10:43 PM

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ export default {
     extend: {
       colors: {
         jennie: '#ec4899',
-        jisoo: '3d2645',
-        roseanne: '000000',
-        lisa: 'e0fbfc',
-        blackpink: 'f0eff4',
+        jisoo: '#3d2645',
+        roseanne: '#000000',
+        lisa: '#e0fbfc',
+        blackpink: '#f0eff4',
       },
     },
   },
