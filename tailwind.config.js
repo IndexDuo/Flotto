a82f5cfbd4f9cced43252c7c71852bcfdbd160a8
ca764763 <112090392+ca764763@users.noreply.github.com>
2023-10-08 09:44:36 +0000

    Oct 8, 2023, 5:44 AM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./map.js"],
+  content: ['./index.html', './map.js', './mapData.js'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
