cb3286fbc8310b324e2603aa2a4dd1798a99192c
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2023-10-08 11:16:31 +0000

    Oct 8, 2023, 7:16 AM

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ import { sanitizedZipcodes, processJsonArray } from './mapData.js'
 // import { sanitizedZipcodes, processJsonArray } from './mapData.js'
 
 // Make an API request to fetch your statistical data from the server
-var zipArray = []
+var zipArray = [];
 
 fetch('http://localhost:3000/getData/winResults/zipcodes')
   .then((response) => {
@@ -20,7 +20,11 @@ fetch('http://localhost:3000/getData/winResults/zipcodes')
       console.error('API response data is not an array.')
       return
     }
-    zipArray = data
+    //data is an array with all the zipcodes. I want to push the zipcodes into zipArray
+
+    // Push the zipcodes into zipArray
+    zipArray = zipArray.concat(data)
+    // console.log(zipArray)
 
     //data is an array with all the zipcodes
     // console.log(data)
@@ -32,33 +36,57 @@ fetch('http://localhost:3000/getData/winResults/zipcodes')
     console.error('Error fetching data from the API:', error)
   })
 
-  // it's not possible to input multiple zipcodes at once into the google maps api, so we need to loop through the array and make a request for each zipcode. for each request, we need to get the lat and lng and add it to the map.
-
 const apiKey = 'AIzaSyCg8cry2Qy-Hgn9c9eEMRjoZeSqsjk4ymc'
 
-fetch(
-  'https://maps.googleapis.com/maps/api/geocode/json?address=${zipString}&key=${apiKey}'
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const coodinates = data.results.map((result) => ({
-      lat: result.geometry.location.lat,
-      lng: result.geometry.location.lng,
-    }))
+// it's not possible to input multiple zipcodes at once into the google maps api, so we need to loop through the array and make a request for each zipcode. for each request, we need to get the lat and lng and add it to an array of coordinates. then we can use the coordinates to add markers to the map.
 
-    var map = L.map('map').setView([28.241, -83.183], 7)
+var coordinates = []
+console.log(zipArray[0])
 
-    //base map layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map)
+zipArray.forEach((zipcode) => {
+  fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      coordinates = data.results.map((result) => ({
+        lat: result.geometry.location.lat,
+        lng: result.geometry.location.lng,
+      }))
+      // Add the coordinates to an array
+      // ...
+    })
+    .catch((error) => {
+      console.error(`Error fetching data for zipcode ${zipcode}:`, error)
+    })
+})
 
-    for (const index in coodinates) {
-      const coord = coordinates[index]
-      L.marker([coord.lat, coord.lng]).addTo(map)
-    }
-  })
-  .catch((error) => {
-    console.error('Error:', error)
-  })
+console.log(coordinates)
+
+// fetch(
+//   'https://maps.googleapis.com/maps/api/geocode/json?address=${zipString}&key=${apiKey}'
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const coodinates = data.results.map((result) => ({
+//       lat: result.geometry.location.lat,
+//       lng: result.geometry.location.lng,
+//     }))
+
+//   var map = L.map('map').setView([28.241, -83.183], 7)
+
+//   //base map layer
+//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution:
+//       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//   }).addTo(map)
+
+//   for (const index in coodinates) {
+//     const coord = coordinates[index]
+//     L.marker([coord.lat, coord.lng]).addTo(map)
+//   }
+// })
+// .catch((error) => {
+//   console.error('Error:', error)
+// })
