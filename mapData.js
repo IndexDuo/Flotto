83b281342197e14633735f58b817beec3036b9ae
ca764763 <112090392+ca764763@users.noreply.github.com>
2023-10-08 04:33:25 +0000

    Oct 8, 2023, 12:33 AM

--- mapData.js
+++ mapData.js
@@ -10,34 +10,43 @@ fs.readFile(jsonFilePath, 'utf8', (err, data) => {
   }
 
   try {
-    const jsonData = JSON.parse(data)
-    console.log(jsonData)
+    const jsonArray = JSON.parse(data)
 
-    //this is where it messes up
-    const buyerAddress = jsonData[0].buyerAddress
-
-    if (typeof buyerAddress !== 'string') {
-      console.error('buyerAddress is not a string.')
+    if (!Array.isArray(jsonArray)) {
+      console.error('JSON data is not an array.')
       return
     }
 
-    let zipcode = ''
-    for (let i = 0; i < buyerAddress.length; i++) {
-      if (/[0-9]/.test(buyerAddress[i])) {
-        zipcode += buyerAddress[i]
-        if (zipcode.length === 5) {
-          break
-        }
-      } else {
-        zipcode = ''
-      }
-    }
+    // 4. Loop through each JSON object
+    jsonArray.forEach((jsonObj, index) => {
+      const buyerAddress = jsonObj.buyerAddress
 
-    if (zipcode.length === 5) {
-      console.log('Found 5 consecutive numbers:', zipcode)
-    } else {
-      console.log('No 5 consecutive numbers found.')
-    }
+      if (typeof buyerAddress !== 'string') {
+        console.error(`buyerAddress at index ${index} is not a string.`)
+        return
+      }
+
+      // 5. Sanitize the buyerAddress string by removing non-digit characters
+      const sanitizedAddress = buyerAddress.replace(/[^0-9]/g, '')
+
+      // 6. Loop through the sanitized string to find 5 consecutive numbers
+      let zipcodes = ''
+      for (let i = 0; i < sanitizedAddress.length; i++) {
+        zipcodes += sanitizedAddress[i]
+        if (zipcodes.length === 5) {
+          break // Stop when you find 5 consecutive numbers
+        }
+      }
+
+      if (consecutiveNumbers.length === 5) {
+        console.log(
+          `Found 5 consecutive numbers at index ${index}:`,
+          consecutiveNumbers
+        )
+      } else {
+        console.log(`No 5 consecutive numbers found at index ${index}.`)
+      }
+    })
   } catch (error) {
     console.error('Error parsing JSON:', error)
   }
