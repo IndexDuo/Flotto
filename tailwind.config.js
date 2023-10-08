cb058b29aeffd21f3d6bf90c19b2fb4550bfa68a
lindsey-nielsen <122649641+lindsey-nielsen@users.noreply.github.com>
2023-10-08 02:40:06 +0000

    Oct 7, 2023, 10:40 PM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-
 export default {
   content: ["./index.html", "./map.js"],
   theme: {
-    extend: {},
-    colors: {
-      'jisoo': '#3d2645',
-      'roseanne': '#000000',
-      'jennie': '#ec4899',
-      'lisa': 'e0fbfc',
-      'blackpink': 'f0eff4',
+    extend: {
+      colors: {
+        'jennie': '#ec4899',
+        'jisoo': '3d2645',
+        'roseanne': '000000',
+        'lisa': 'e0fbfc',
+        'blackpink': 'f0eff4',
+      },
     },
   },
   plugins: [],
